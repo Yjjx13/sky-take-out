b283e01b65069bc228549f4f51a83778774b8004
叶俊杰 <1225187825@qq.com>
2024-07-14 11:37:14 +0800

    苍穹外卖day2 update

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -88,5 +88,12 @@ public class EmployeeController {
         PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);
         return Result.success(pageResult);
     }
+    @PostMapping("/status/{status}")
+    @ApiOperation(value = "启用禁用员工账号")
+    public Result startOrStop(@PathVariable() Integer status, Long id) {
+        log.info("启用禁用员工账号：{},{}", id,status);
+        employeeService.startOrStop(status, id);
+        return Result.success();
+    }
 
 }
