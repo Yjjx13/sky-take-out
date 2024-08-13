39e1efc154feb8b1a3f3a8482c8ec9f21a11240f
叶俊杰 <1225187825@qq.com>
2024-08-13 08:59:29 +0800

    完结！

--- sky-server/src/main/java/com/sky/config/RedisConfiguration.java
+++ sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -3,19 +3,26 @@ package com.sky.config;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @Slf4j
+@EnableCaching
 public class RedisConfiguration {
+    /**
+     * 配置RedisTemplate，用于操作Redis数据库。
+     * 使用String作为key和value的类型，以便在Redis中存储和检索字符串数据。
+     *
+     * @param factory RedisConnectionFactory的实例，用于创建与Redis服务器的连接。
+     * @return RedisTemplate实例，配置了字符串类型的key和value序列化器。
+     */
     @Bean
     public RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         log.info("开始创建RedisTemplate对象...");
@@ -28,12 +35,5 @@ public class RedisConfiguration {
         log.info("创建RedisTemplate对象成功！");
         return redisTemplate;
     }
-    public RedisSerializer<Object> redisSerializer() {
-        //创建JSON序列化器
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        //必须设置，否则无法序列化实体类对象
-        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
-        return new GenericJackson2JsonRedisSerializer(objectMapper);
-    }
+
 }
